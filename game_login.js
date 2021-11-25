55fe786fce3948f5511f94370374656cef6fcdf3
penmate90 <84581597+penmate90@users.noreply.github.com>
2021-11-25 19:03:25 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,14 @@
+player_1=0;
+player_2=0;
+
+function login_quiz() {
+
+
+
+    player_1=document.getElementById("player1_name").value;
+    localStorage.setItem("player1", player_1)
+    player_1=document.getElementById("player1_name").value;
+    localStorage.setItem("player2", player_2)
+
+    window.location="quiz.html";
+}
